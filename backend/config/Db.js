07e384c34e40bacdc267d5f0f79e7bf55cca3a0e
VaishakAnchan <anchanvaishak619@gmail.com>
2025-04-23 21:45:41 +0530

    db changed

--- backend/config/Db.js
+++ backend/config/Db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 dotenv.config()
-const MongoDB = process.env.MongoDB_URL;
+const MongoDB = process.env.Mongo_URI;
 
 export const connectDB = async()=>{
     try {
